24a2c0be0afdba76af900013c0fc06372875a0d8
Josh Johansen <josh@johansen.us.com>
2018-10-30 21:38:54 -0700

    Updates

--- precache-manifest.39764c1db8655d3c317005d8369f3d71.js
+++ precache-manifest.39764c1db8655d3c317005d8369f3d71.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
   },
   {
-    "revision": "712c11031fc67603d503",
-    "url": "/contactmanager/static/js/main.712c1103.chunk.js"
+    "revision": "559e061fc80db2b07ff1",
+    "url": "/contactmanager/static/js/main.559e061f.chunk.js"
   },
   {
     "revision": "0061def1f9b55d9b19e3",
     "url": "/contactmanager/static/js/1.0061def1.chunk.js"
   },
   {
-    "revision": "712c11031fc67603d503",
+    "revision": "559e061fc80db2b07ff1",
     "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/contactmanager/static/css/1.1d83608d.chunk.css"
   },
   {
-    "revision": "2c5baddea0f587f9f1c2fb33e095fbd7",
+    "revision": "bf8dbff69c4bd26c38fb4071089833ae",
     "url": "/contactmanager/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/contactmanager/precache-manifest.dfd4f3aaf63ba8d2631b2f138e129545.js"
+  "/contactmanager/precache-manifest.39764c1db8655d3c317005d8369f3d71.js"
 );
 
 workbox.clientsClaim();
